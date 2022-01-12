cfdbd98c30359303275bfe9315e75914d0980bbf
Sarthak Madan <69874181+SarthakMadan6@users.noreply.github.com>
2022-01-11 20:19:04 -0800

    Add files via upload

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Sarthak Madan on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
